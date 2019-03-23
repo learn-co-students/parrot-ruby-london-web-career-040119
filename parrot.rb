43841ea41e7d16b168619888a8fae87644c11a2e
Endy Ranaudo <endyranaudo@gmail.com>
2019-03-23 17:28:46 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(arg = "Squawk!" )
-  ret = puts arg
-  arg
+def parrot(str = "Squawk!")
+  puts str
+  str
 end
